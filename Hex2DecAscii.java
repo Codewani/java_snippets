91b2dc18767491e3649d25587820db0bc8a196ba
Codewani <kondwanimwape013@gmail.com>
2023-06-28 14:53:24 +0200

    more code

---
+++ Hex2DecAscii.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Hex2DecAscii{
+	public static void main(String[] args){
+		System.out.println("Enter a valid hexadecimal value");
+		Scanner input = new Scanner(System.in);
+		String s = input.next().toUpperCase();
+		double x = 0;
+		int n = s.length() - 1;
+		for(int i = 0; i <= n; i++){
+			char hex = s.charAt(i);
+			int value = (hex >= '0' && hex <= '9')? (int)(hex) - 48: (int)(hex) - 55;
+			x += (value * Math.pow(16, n-i));     
+		}
+		System.out.println((int)x);
+	}
+}
