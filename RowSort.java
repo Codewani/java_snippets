ca7f440d14ece4a48994b08ab5633b66183a9414
Codewani <kondwanimwape013@gmail.com>
2023-07-13 18:25:12 +0200

    RowSort

--- RowSort.java
+++ RowSort.java
@@ -1,5 +1,17 @@
 import java.util.*;
-
+/**
+ * program that prompts the user to enter a 3 * 3 matrix of double 
+ * values and displays a new row-sorted matrix. Here is a sample run:
+ * Enter a 3-by-3 matrix row by row: 
+ * 0.15 0.875 0.375
+ * 0.55 0.005 0.225
+ * 0.30 0.12 0.4
+ *
+ * The row-sorted array is 
+ * 0.15 0.375 0.875
+ * 0.005 0.225 0.55 
+ * 0.12 0.30 0.4
+ */
 public class RowSort{
 	public static void main(String[] args){
 		double[][] myArray = new double[3][3];
