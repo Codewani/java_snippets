5fb695bf448af22c5a1a064ca49edc90c35fa4ae
Codewani <kondwanimwape013@gmail.com>
2023-09-27 20:41:10 +0200

    LAb 2

--- tax.java
+++ tax.java
@@ -4,23 +4,26 @@ public class tax{
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter your income: ");
 		double income = input.nextDouble();
+		double first_bracket = 4000, second_bracket = 6800, third_bracket = 8900;
+		//percentages of each bracket
+		double perc_brack1 = 0.00, perc_brack2 = 0.200, perc_brack3 = 0.300, perc_brack4 = 0.375;
 		if(income <= 4000.00){
-			double tax = income * 0.000;
+			double tax = income * perc_brack1;
 			double net_salary = income - tax;
 			System.out.println("Tax: " + tax + " Net salary: " + net_salary);
 		}
 		else if(income <= 6800.00){
-			double tax = (4000 * 0.000) + (income - 4000) * 0.200;
+			double tax = (first_bracket * perc_brack1) + (income - first_bracket) * perc_brack2;
 			double net_salary = income - tax;
 			System.out.println("Tax: " + tax + " Net salary: " + net_salary);
 		}
 		else if(income <= 8900){
-			double tax = (4000 * 0.000) + ((6800 - 4000) * 0.200) + (income - 6800) * 0.300;
+			double tax = (first_bracket * perc_brack1) + ((second_bracket - first_bracket) * perc_brack2) + (income - second_bracket) * perc_brack3;
 			double net_salary = income - tax;
 			System.out.println("Tax: " + tax + " Net salary: " + net_salary);
 		}
 		else{
-			double tax = (4000 * 0.000) + ((6800 - 4000) * 0.200) + ((8900 - 6800) * 0.300) + (income - 8900) * 0.375;
+			double tax = (first_bracket * perc_brack1) + ((second_bracket - first_bracket) * perc_brack2) + ((third_bracket - second_bracket) * perc_brack3) + (income - third_bracket) * perc_brack4;
 			double net_salary = income - tax;
 			System.out.println("Tax: " + tax + " Net salary: " + net_salary);
 		}
